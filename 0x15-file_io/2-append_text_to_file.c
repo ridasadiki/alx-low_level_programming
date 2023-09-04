4b7a2730444991cce6c87be6b328561a7ffbc524
ridasadiki <mohamedridasadiki1@gmail.com>
2023-09-04 10:14:52 -0700

    file i/o

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -1,39 +1,42 @@
 #include "main.h"
 
 /**
- * append_text_to_file - appends text at the end of a file
- * @filename: filename.
- * @text_content: added content.
+ * _strlen - return the length of string
+ * @s: string that we will check length
  *
- * Return: 1 if the file exists. -1 if the fails does not exist
- * or if it fails.
+ * Return: intg length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * append_text_to_file - appends text to a file
+ * @filename: name of the file to create
+ * @text_content: text to write
+ *
+ * Return: 1 on success 0 on fail
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int fd;
-	int nletters;
-	int rwr;
+	ssize_t bytes = 0, len = _strlen(text_content);
 
 	if (!filename)
 		return (-1);
-
 	fd = open(filename, O_WRONLY | O_APPEND);
-
 	if (fd == -1)
 		return (-1);
-
-	if (text_content)
-	{
-		for (nletters = 0; text_content[nletters]; nletters++)
-			;
-
-		rwr = write(fd, text_content, nletters);
-
-		if (rwr == -1)
-			return (-1);
-	}
-
+	if (len)
+		bytes = write(fd, text_content, len);
 	close(fd);
-
-	return (1);
+	return (bytes == len ? 1 : -1);
 }
