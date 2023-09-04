41e6ddeb0e21e45b4d9f4ae93d90cf30ff748f21
ridasadiki <mohamedridasadiki1@gmail.com>
2023-09-04 08:59:12 -0700

    file i/o

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,10 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <fcntl.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+int create_file(const char *filename, char *text_content);
+int create_file(const char *filename, char *text_content);
+ssize_t read_textfile(const char *filename, size_t letters);
+#endif
